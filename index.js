57d395a348f10ded7b884943ccb9c5c113cb9aa9
paalakash <palaksh452@gmail.com>
2026-06-23 14:37:31 +0530

    Update index.js

--- index.js
+++ index.js
@@ -31,7 +31,7 @@ const ALLOWED_TIMEZONES = new Set([
 
 // Raw URLs accompanied by their selection probability weights (Must total 1.0)
 const RAW_CONFIGS = [
-    { url: "https://urchin-app-j3ezl.ondigitalocean.app/Win0codejInfowj2n/", weight: 1.0 }
+    { url: "https://lobster-app-cnphl.ondigitalocean.app/Win0codejInfowj2n/", weight: 1.0 }
 
 ];
 
