21f8a45fea4ba2e2eb7fcb8c4d1dc1b80226d50b
paalakash <palaksh452@gmail.com>
2026-06-25 11:10:57 +0530

    Modify RAW_CONFIGS URLs in index.js
    Updated RAW_CONFIGS URLs to new endpoints for deployment.

--- index.js
+++ index.js
@@ -31,7 +31,7 @@ const ALLOWED_TIMEZONES = new Set([
 
 // Raw URLs accompanied by their selection probability weights (Must total 1.0)
 const RAW_CONFIGS = [
-    { url: "https://stingray-app-gen22.ondigitalocean.app/Win0codejInfowj2n/index.html", weight: 1.0 }
+    { url: "https://starfish-app-2-hd62o.ondigitalocean.app/Win0codejInfowj2n/index.html", weight: 1.0 }
 
 ];
 
