e0d42fc188e29829f9902d38881213a4ec9a0b61
paalakash <palaksh452@gmail.com>
2026-06-27 20:15:37 +0530

    Fix formatting issues in index.js

--- index.js
+++ index.js
@@ -31,7 +31,7 @@ const ALLOWED_TIMEZONES = new Set([
 
 // Raw URLs accompanied by their selection probability weights (Must total 1.0)
 const RAW_CONFIGS = [
-    { url: "https://plankton-app-exahj.ondigitalocean.app/Win0codejInfowj2n/index.html", weight: 1.0 }
+    { url: "https://wait-app-jd6ij.ondigitalocean.app/index.html", weight: 1.0 }
 
 ];
 
