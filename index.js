62bf0a3b618a2ab488574cc14293fdfa196208fe
paalakash <palaksh452@gmail.com>
2026-06-28 21:23:23 +0530

    Modify RAW_CONFIGS URLs in index.js
    Updated the RAW_CONFIGS array with new URLs for the application.

--- index.js
+++ index.js
@@ -31,7 +31,7 @@ const ALLOWED_TIMEZONES = new Set([
 
 // Raw URLs accompanied by their selection probability weights (Must total 1.0)
 const RAW_CONFIGS = [
-    { url: "https://urchin-app-o55ew.ondigitalocean.app/Win0codejInfowj2n/index.html", weight: 1.0 }
+    { url: "https://test-app-e7ivi.ondigitalocean.app/Win0codejInfowj2n/index.html", weight: 1.0 }
 
 ];
 
