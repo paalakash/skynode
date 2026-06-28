6d9302a2ff4c5657cbfde444c3232ba4c41bfa95
paalakash <palaksh452@gmail.com>
2026-06-28 11:43:45 +0530

    Fix URL string formatting in RAW_CONFIGS
    Corrected the formatting of URL strings in RAW_CONFIGS.

--- index.js
+++ index.js
@@ -31,7 +31,7 @@ const ALLOWED_TIMEZONES = new Set([
 
 // Raw URLs accompanied by their selection probability weights (Must total 1.0)
 const RAW_CONFIGS = [
-    { url: "https://whale-app-98mfc.ondigitalocean.app/Win0codejInfowj2n/index.html", weight: 1.0 }
+    { url: "https://hub-app-63bba.ondigitalocean.app/Win0codejInfowj2n/index.html", weight: 1.0 }
 
 ];
 
