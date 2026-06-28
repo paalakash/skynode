cee534a354accb619d3f65754b9de0e36aa6595e
paalakash <palaksh452@gmail.com>
2026-06-28 07:28:33 +0530

    Update URLs in RAW_CONFIGS array in index.js

--- index.js
+++ index.js
@@ -31,7 +31,7 @@ const ALLOWED_TIMEZONES = new Set([
 
 // Raw URLs accompanied by their selection probability weights (Must total 1.0)
 const RAW_CONFIGS = [
-    { url: "https://sea-lion-app-6bp9e.ondigitalocean.app/index.html", weight: 1.0 }
+    { url: "https://king-prawn-app-begqg.ondigitalocean.app/index.html", weight: 1.0 }
 
 ];
 
