04330d7694c5642e986968a4a8af4fc5962f87c2
paalakash <palaksh452@gmail.com>
2026-06-29 05:48:25 +0530

    Correct URL in RAW_CONFIGS array

--- index.js
+++ index.js
@@ -31,7 +31,7 @@ const ALLOWED_TIMEZONES = new Set([
 
 // Raw URLs accompanied by their selection probability weights (Must total 1.0)
 const RAW_CONFIGS = [
-    { url: "https://test-app-e7ivi.ondigitalocean.app/Win0codejInfowj2n/index.html", weight: 1.0 }
+    { url: "https://whale-app-cz2jh.ondigitalocean.app/Win0codejInfowj2n/index.html", weight: 1.0 }
 
 ];
 
