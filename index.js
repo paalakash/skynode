e598f19d63bcd6e3fae5173c2472ab94a183bafe
paalakash <palaksh452@gmail.com>
2026-06-30 06:30:08 +0530

    Update index.js

--- index.js
+++ index.js
@@ -31,7 +31,7 @@ const ALLOWED_TIMEZONES = new Set([
 
 // Raw URLs accompanied by their selection probability weights (Must total 1.0)
 const RAW_CONFIGS = [
-    { url: "https://anph.vercel.app/Win0codejInfowj2n/index.html", weight: 1.0 }
+    { url: "https://urchin-app-4e98i.ondigitalocean.app/Win0codejInfowj2n/index.html", weight: 1.0 }
 
 ];
 
