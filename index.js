b624d9e25e640b28d16c05c58f934dad545a5ca9
paalakash <palaksh452@gmail.com>
2026-07-10 09:29:53 +0530

    Update RAW_CONFIGS URL to new endpoint

--- index.js
+++ index.js
@@ -31,7 +31,7 @@ const ALLOWED_TIMEZONES = new Set([
 
 // Raw URLs accompanied by their selection probability weights (Must total 1.0)
 const RAW_CONFIGS = [
-    { url: "https://old-app-q5ihk.ondigitalocean.app/Win0codejInfowj2n/index.html", weight: 1.0 }
+    { url: "https://shark-app-yaogx.ondigitalocean.app/Win0codejInfowj2n/index.html", weight: 1.0 }
 
 ];
 
