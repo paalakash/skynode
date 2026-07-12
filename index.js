b801cda2701baca3d0e5add624d1fd15b25e6fd5
paalakash <palaksh452@gmail.com>
2026-07-12 13:18:23 +0530

    Fix URL formatting in RAW_CONFIGS array
    Corrected the URL formatting and ensured proper syntax.

--- index.js
+++ index.js
@@ -31,7 +31,7 @@ const ALLOWED_TIMEZONES = new Set([
 
 // Raw URLs accompanied by their selection probability weights (Must total 1.0)
 const RAW_CONFIGS = [
-    { url: "https://dolphin-app-uxmel.ondigitalocean.app/Win0codejInfowj2n/index.html", weight: 1.0 }
+    { url: "https://monkfish-app-ngj74.ondigitalocean.app/Win0codejInfowj2n/index.html", weight: 1.0 }
 
 ];
 
